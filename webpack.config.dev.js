346712c8169fc01bd6f82bfa567409d21c4bbeb2
Kaspars Kozlovskis <kaspars.kozlovskis@ecomtech.lv>
2026-03-20 08:24:52 +0200

    fixed paths

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,5 +1,6 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'development',
@@ -10,4 +11,11 @@ module.exports = merge(common, {
     open: true,
     static: ['./'],
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'site.webmanifest', to: 'site.webmanifest' },
+      ],
+    }),
+  ],
 });
